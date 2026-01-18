3d3c69b734134f51aebf1d90c9ca08d5d050799b
BelieveMko <mukobelieve@gmail.com>
2026-01-18 20:57:53 +0200

    Advanced relationship models in Django

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,20 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+
+#books by  author
+def get_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    return author.books.all()
+
+
+#List books
+def get_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
+
+
+#Get the librarian
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.librarian
+
