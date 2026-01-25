7615253cc2836f5b1715ddf2f70a450c2bc85f99
BelieveMko <mukobelieve@gmail.com>
2026-01-25 21:27:20 +0200

    Task 1 matching imports

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,28 +1,27 @@
 from django.urls import path
-from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import (
+    list_books,
+    LibraryDetailView,
+    register,
+    admin_view,
+    librarian_view,
+    member_view,
+)
 
 urlpatterns = [
-   
-    path('books/', views.list_books, name='list_books'),
-    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    # Question 1 views (THIS IS WHAT THE ERROR IS ABOUT)
+    path('books/', list_books, name='list_books'),
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
-    path('register/', views.register, name='register'),
+    # Authentication
+    path('register/', register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 
-	path('admin-view/', views.admin_view, name='admin_view'),
-    path('librarian-view/', views.librarian_view, name='librarian_view'),
-    path('member-view/', views.member_view, name='member_view'),
-
-    path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-
-  
-    path('books/', views.list_books, name='list_books'),
-    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-
-
+    # Role-based views
+    path('admin-view/', admin_view, name='admin_view'),
+    path('librarian-view/', librarian_view, name='librarian_view'),
+    path('member-view/', member_view, name='member_view'),
 ]
 
