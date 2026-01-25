3564d4bd17c695399de0fd6b5d864825814979f3
BelieveMko <mukobelieve@gmail.com>
2026-01-25 21:31:32 +0200

    Task 1 matching imports

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,25 +1,26 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books
+from .views import   LibraryDetailView
 from .views import (
-    list_books,
-    LibraryDetailView,
     register,
     admin_view,
     librarian_view,
     member_view,
 )
 
+
 urlpatterns = [
-    # Question 1 views (THIS IS WHAT THE ERROR IS ABOUT)
+    
     path('books/', list_books, name='list_books'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
-    # Authentication
+   
     path('register/', register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 
-    # Role-based views
+  
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
