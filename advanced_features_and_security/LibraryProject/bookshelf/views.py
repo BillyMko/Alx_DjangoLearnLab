744a10f8c3a8a16ac75a4e873e5e4b8f0c30332f
BelieveMko <mukobelieve@gmail.com>
2026-02-01 21:02:12 +0200

    inserting  security practices

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -13,7 +13,10 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from .forms import BookForm
 
 User = get_user_model()
 
@@ -145,3 +148,25 @@ def delete_book(request, book_id):
         book.delete()
         return redirect('book_list')
     return render(request, 'bookshelf/delete_book.html', {'book': book})
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def form_example(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+
+    return render(request, 'bookshelf/form_example.html', {'form': form})
+
+def add_csp_headers(response):
+    response['Content-Security-Policy'] = "default-src 'self'"
+    return response
