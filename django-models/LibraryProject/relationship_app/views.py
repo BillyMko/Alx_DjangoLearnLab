e22a599abad730f9a50278445419837a7cc89a66
BelieveMko <mukobelieve@gmail.com>
2026-02-04 10:31:40 +0200

    update views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -71,32 +71,15 @@ class LibraryDetailView(DetailView):
 
 @permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request):
-    if request.method == "POST":
-        title = request.POST.get('title')
-        author_id = request.POST.get('author_id')
-        if title and author_id:
-            author = get_object_or_404(Author, id=author_id)
-            Book.objects.create(title=title, author=author)
-            return redirect('list_books')
     return render(request, 'relationship_app/add_book.html')
 
 
 @permission_required('relationship_app.can_change_book', raise_exception=True)
 def edit_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-    if request.method == "POST":
-        title = request.POST.get('title')
-        if title:
-            book.title = title
-            book.save()
-            return redirect('list_books')
+    
     return render(request, 'relationship_app/edit_book.html', {'book': book})
 
 
 @permission_required('relationship_app.can_delete_book', raise_exception=True)
 def delete_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-    if request.method == "POST":
-        book.delete()
-        return redirect('list_books')
     return render(request, 'relationship_app/delete_book.html', {'book': book})
