4cff4d61d04114b6006a100b6e0a4157f431e031
BelieveMko <mukobelieve@gmail.com>
2026-02-04 09:37:28 +0200

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -48,9 +48,9 @@ class Librarian(models.Model):
 
 class UserProfile(models.Model):
     ROLE_CHOICES = [
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
+        ('Admin'),
+        ('Librarian'),
+        ('Member'),
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
