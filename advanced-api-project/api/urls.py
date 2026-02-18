b0a0ee299cb79a74bbb6a4d814b13288ab0013ee
BelieveMko <mukobelieve@gmail.com>
2026-02-18 23:01:30 +0200

    Building Custom Views and Generic Views

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('books/create/', BookCreateView.as_view(), name='bookcreate'),
     path('books/<int:pk>/update/', BookUpdateView.as_view(), name='bookupdate'),
     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='bookdelete'),
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
 
