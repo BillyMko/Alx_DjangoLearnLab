487ba52b2a863a7c8a06fb54514e164da7d8009d
BelieveMko <mukobelieve@gmail.com>
2026-02-19 13:07:36 +0200

    configured to use mysql database

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blogdatabase',
+        'USER': 'charity',
+        'PASSWORD': 'billy2001.',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
