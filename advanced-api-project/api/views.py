857dd233ed3ee1c0464e6d305dbdeb4fb72af939
BelieveMko <mukobelieve@gmail.com>
2026-02-19 07:30:27 +0200

    Importing the modules as directed by ALX checker

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -15,7 +15,7 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]
-    filter_backends = [DjangoFilterBackend,  SearchFilter, filters.OrderingFilter]
+    filter_backends = [DjangoFilterBackend,  filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['title', 'author', 'publication_year']
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year']
